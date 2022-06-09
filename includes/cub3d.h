b3b6471d6fd61468dd3ee705c7e878697167c7b0
Manon PRIGENT <mprigent@e1r2p7.42.fr>
2022-06-09 17:31:07 +0200

    norme last minute

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: mprigent <mprigent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 16:23:29 by gadeneux          #+#    #+#             */
-/*   Updated: 2022/06/09 17:05:19 by mprigent         ###   ########.fr       */
+/*   Updated: 2022/06/09 17:28:11 by mprigent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,28 +70,28 @@ typedef struct s_texture {
 }				t_texture;
 
 typedef struct s_raycasting {
-	double		cameraX;
-	double		rayDirX;
-	double		rayDirY;
-	int			mapX;
-	int			mapY;
-	double		sideDistX;
-	double		sideDistY;
-	double		deltaDistX;
-	double		deltaDistY;
-	double		perpWallDist;
-	int			stepX;
-	int			stepY;
+	double		camera_x;
+	double		ray_dir_x;
+	double		ray_dir_y;
+	int			map_x;
+	int			map_y;
+	double		side_dist_x;
+	double		side_dist_y;
+	double		delta_dist_x;
+	double		delta_dist_y;
+	double		perp_wall_dist;
+	int			step_x;
+	int			step_y;
 	int			hit;
 	int			side;
-	int			lineHeight;
-	int			drawStart;
-	int			drawEnd;
-	double		wallX;
+	int			line_height;
+	int			draw_start;
+	int			draw_end;
+	double		wall_x;
 	t_texture	*tex;
-	int			texX;
+	int			tex_x;
 	double		step;
-	double		texPos;
+	double		tex_pos;
 }				t_raycasting;
 
 typedef struct s_game {
--- srcs/raycasting.c
+++ srcs/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: mprigent <mprigent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 00:00:22 by gadeneux          #+#    #+#             */
-/*   Updated: 2022/06/09 02:16:04 by mprigent         ###   ########.fr       */
+/*   Updated: 2022/06/09 17:28:59 by mprigent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,27 @@
 
 void	calculate_initial_side_dist(t_game *game, t_raycasting *ray)
 {
-	if (ray->rayDirX < 0)
+	if (ray->ray_dir_x < 0)
 	{
-		ray->stepX = -1;
-		ray->sideDistX = (game->player_x - ray->mapX) * ray->deltaDistX;
+		ray->step_x = -1;
+		ray->side_dist_x = (game->player_x - ray->map_x) * ray->delta_dist_x;
 	}
 	else
 	{
-		ray->stepX = 1;
-		ray->sideDistX = (ray->mapX + 1.0 - game->player_x) * ray->deltaDistX;
+		ray->step_x = 1;
+		ray->side_dist_x = (ray->map_x + 1.0 - game->player_x) \
+			* ray->delta_dist_x;
 	}
-	if (ray->rayDirY < 0)
+	if (ray->ray_dir_y < 0)
 	{
-		ray->stepY = -1;
-		ray->sideDistY = (game->player_y - ray->mapY) * ray->deltaDistY;
+		ray->step_y = -1;
+		ray->side_dist_y = (game->player_y - ray->map_y) * ray->delta_dist_y;
 	}
 	else
 	{
-		ray->stepY = 1;
-		ray->sideDistY = (ray->mapY + 1.0 - game->player_y) * ray->deltaDistY;
+		ray->step_y = 1;
+		ray->side_dist_y = (ray->map_y + 1.0 - game->player_y) \
+			* ray->delta_dist_y;
 	}
 }
 
@@ -40,52 +42,52 @@ void	perform_dda(t_game *game, t_raycasting *ray)
 {
 	while (ray->hit == 0)
 	{
-		if (ray->sideDistX < ray->sideDistY)
+		if (ray->side_dist_x < ray->side_dist_y)
 		{
-			ray->sideDistX += ray->deltaDistX;
-			ray->mapX += ray->stepX;
+			ray->side_dist_x += ray->delta_dist_x;
+			ray->map_x += ray->step_x;
 			ray->side = 0;
 		}
 		else
 		{
-			ray->sideDistY += ray->deltaDistY;
-			ray->mapY += ray->stepY;
+			ray->side_dist_y += ray->delta_dist_y;
+			ray->map_y += ray->step_y;
 			ray->side = 1;
 		}
-		if (ft_iswall(game, ray->mapX, ray->mapY))
+		if (ft_iswall(game, ray->map_x, ray->map_y))
 			ray->hit = 1;
 	}
 	if (ray->side == 0)
-		ray->perpWallDist = (ray->sideDistX - ray->deltaDistX);
+		ray->perp_wall_dist = (ray->side_dist_x - ray->delta_dist_x);
 	else
-		ray->perpWallDist = (ray->sideDistY - ray->deltaDistY);
-	ray->lineHeight = (int)(SCREEN_HEIGHT / ray->perpWallDist);
+		ray->perp_wall_dist = (ray->side_dist_y - ray->delta_dist_y);
+	ray->line_height = (int)(SCREEN_HEIGHT / ray->perp_wall_dist);
 }
 
 void	calculate_texture_coordinates(t_game *game, t_raycasting *ray)
 {
-	ray->drawStart = -ray->lineHeight / 2 + SCREEN_HEIGHT / 2;
-	if (ray->drawStart < 0)
-		ray->drawStart = 0;
-	ray->drawEnd = ray->lineHeight / 2 + SCREEN_HEIGHT / 2;
-	if (ray->drawEnd >= SCREEN_HEIGHT)
-		ray->drawEnd = SCREEN_HEIGHT - 1;
+	ray->draw_start = -ray->line_height / 2 + SCREEN_HEIGHT / 2;
+	if (ray->draw_start < 0)
+		ray->draw_start = 0;
+	ray->draw_end = ray->line_height / 2 + SCREEN_HEIGHT / 2;
+	if (ray->draw_end >= SCREEN_HEIGHT)
+		ray->draw_end = SCREEN_HEIGHT - 1;
 	if (ray->side == 0)
-		ray->wallX = game->player_y + ray->perpWallDist * ray->rayDirY;
+		ray->wall_x = game->player_y + ray->perp_wall_dist * ray->ray_dir_y;
 	else
-		ray->wallX = game->player_x + ray->perpWallDist * ray->rayDirX;
-	ray->wallX -= floor(ray->wallX);
+		ray->wall_x = game->player_x + ray->perp_wall_dist * ray->ray_dir_x;
+	ray->wall_x -= floor(ray->wall_x);
 	if (ray->side == 0)
-		ray->tex = get_texture(game, get_direction(ray->side, ray->stepX));
+		ray->tex = get_texture(game, get_direction(ray->side, ray->step_x));
 	else
-		ray->tex = get_texture(game, get_direction(ray->side, ray->stepY));
-	ray->texX = (int)(ray->wallX * (double) ray->tex->width);
-	if (ray->side == 0 && ray->rayDirX > 0)
-		ray->texX = ray->tex->width - ray->texX - 1;
-	if (ray->side == 1 && ray->rayDirY < 0)
-		ray->texX = ray->tex->width - ray->texX - 1;
-	ray->step = 1.0 * ray->tex->height / ray->lineHeight;
-	ray->texPos = (ray->drawStart - SCREEN_HEIGHT / 2 + ray->lineHeight / 2)
+		ray->tex = get_texture(game, get_direction(ray->side, ray->step_y));
+	ray->tex_x = (int)(ray->wall_x * (double) ray->tex->width);
+	if (ray->side == 0 && ray->ray_dir_x > 0)
+		ray->tex_x = ray->tex->width - ray->tex_x - 1;
+	if (ray->side == 1 && ray->ray_dir_y < 0)
+		ray->tex_x = ray->tex->width - ray->tex_x - 1;
+	ray->step = 1.0 * ray->tex->height / ray->line_height;
+	ray->tex_pos = (ray->draw_start - SCREEN_HEIGHT / 2 + ray->line_height / 2)
 		* ray->step;
 }
 
@@ -94,22 +96,22 @@ void	draw_3d_view(t_game *game, t_raycasting *ray, int rayon_x)
 	int	y;
 	int	i;
 
-	y = ray->drawStart;
-	while (y < ray->drawEnd)
+	y = ray->draw_start;
+	while (y < ray->draw_end)
 	{
 		ft_pixel(game->img, rayon_x, y, ft_pixel_color(ray->tex->data,
-				ray->texX, ray->texPos));
-		ray->texPos += ray->step;
+				ray->tex_x, ray->tex_pos));
+		ray->tex_pos += ray->step;
 		y++;
 	}
 	i = 0;
-	while (i < ray->drawStart)
+	while (i < ray->draw_start)
 	{
 		ft_pixel(game->img, rayon_x, i, game->ceil);
 		i++;
 	}
-	i = ray->drawEnd;
-	while (i < ray->drawEnd + SCREEN_HEIGHT)
+	i = ray->draw_end;
+	while (i < ray->draw_end + SCREEN_HEIGHT)
 	{
 		ft_pixel(game->img, rayon_x, i, game->floor);
 		i++;
@@ -123,19 +125,19 @@ void	draw_rays_3d(t_game *game, t_raycasting *ray)
 	rayon_x = 0;
 	while (rayon_x < SCREEN_WIDTH)
 	{
-		ray->cameraX = 2 * rayon_x / (double) SCREEN_WIDTH - 1;
-		ray->rayDirX = game->dir_x + game->plane_x * ray->cameraX;
-		ray->rayDirY = game->dir_y + game->plane_y * ray->cameraX;
-		ray->mapX = (int) game->player_x;
-		ray->mapY = (int) game->player_y;
-		if (ray->rayDirX == 0)
-			ray->deltaDistX = 1e30;
+		ray->camera_x = 2 * rayon_x / (double) SCREEN_WIDTH - 1;
+		ray->ray_dir_x = game->dir_x + game->plane_x * ray->camera_x;
+		ray->ray_dir_y = game->dir_y + game->plane_y * ray->camera_x;
+		ray->map_x = (int) game->player_x;
+		ray->map_y = (int) game->player_y;
+		if (ray->ray_dir_x == 0)
+			ray->delta_dist_x = 1e30;
 		else
-			ray->deltaDistX = fabs(1 / ray->rayDirX);
-		if (ray->rayDirY == 0)
-			ray->deltaDistY = 1e30;
+			ray->delta_dist_x = fabs(1 / ray->ray_dir_x);
+		if (ray->ray_dir_y == 0)
+			ray->delta_dist_y = 1e30;
 		else
-			ray->deltaDistY = fabs(1 / ray->rayDirY);
+			ray->delta_dist_y = fabs(1 / ray->ray_dir_y);
 		ray->hit = 0;
 		calculate_initial_side_dist(game, ray);
 		perform_dda(game, ray);
